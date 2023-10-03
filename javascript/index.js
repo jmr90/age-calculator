1bcb2cccde1055c6255268e05b70d4cd5e643b84
jmr90 <jonathanmartinez@Jonathans-MacBook-Pro.local>
2023-10-03 13:41:38 -0400

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form');
+const birthdateInput = document.querySelector('#birthdate');
+const ageOutput = document.querySelector('#age');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const birthdateString = birthdateInput.value;
+
+  const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+  if (!dateRegex.test(birthdateString)) {
+    alert('Please enter a valid birthdate in MM/DD/YYYY format.');
+    return;
+  }
+
+  const birthdate = new Date(birthdateString);
+
+  const ageInMilliseconds = Date.now() - birthdate.getTime();
+  const ageDate = new Date(ageInMilliseconds);
+  const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+  ageOutput.textContent = `You are ${age} years old.`;
+});
+
+form.addEventListener('reset', function() {
+  ageOutput.textContent = null;
+})
